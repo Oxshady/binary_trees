263b1c03b4b3a736ac467c2acab1dae47ee43831
Oxshady <shadymahmod55@gmail.com>
2024-06-05 19:25:39 -0700

    prototype of binary tree node function

--- binary_trees.h
+++ binary_trees.h
@@ -17,5 +17,6 @@ struct binary_tree_s
 };
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 #endif
 
